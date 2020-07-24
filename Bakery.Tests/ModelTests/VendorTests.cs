1833ac8f984704e290d6a877f57aa0e3abbf0c3a
Erich Richter <erich.richter@gmail.com>
2020-07-24 12:01:58 -0700

    add failed test to get vendor name

--- Bakery.Tests/ModelTests/VendorTests.cs
+++ Bakery.Tests/ModelTests/VendorTests.cs
@@ -21,5 +21,20 @@ namespace Bakery.Tests
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
 
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      //Arrange
+      string name = "Test Vendor";
+      Vendor newVendor = new Vendor(name);
+
+      //Act
+      string result = newVendor.Name;
+
+      //Assert
+      Assert.AreEqual(name, result);
+    }
+  
+  
   }
 }
--- Bakery/Models/Vendor.cs
+++ Bakery/Models/Vendor.cs
@@ -5,13 +5,13 @@ namespace Bakery.Models
   public class Vendor
   {
     private static List<Vendor> _instances = new List<Vendor> { };
-    public string Name { get; set; }
+    public string Name {get; set; }
     public int Id { get; }
     public List<Order> Orders { get; set; }
 
     public Vendor(string vendorName)
     {
-      Name = vendorName;
+      //Name = vendorName;
       _instances.Add(this);
       Id = _instances.Count;
       Orders = new List<Order> { };
