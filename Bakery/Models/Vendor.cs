3fbb81d293ee8d2a02139ac638e5a459cfae7346
Erich Richter <erich.richter@gmail.com>
2020-07-24 11:53:08 -0700

    passed test to create instance of vendor

--- Bakery/Models/Vendor.cs
+++ Bakery/Models/Vendor.cs
@@ -9,13 +9,13 @@ namespace Bakery.Models
     public int Id { get; }
     public List<Order> Orders { get; set; }
 
-    // public Vendor(string vendorName)
-    // {
-    //   Name = vendorName;
-    //   _instances.Add(this);
-    //   Id = _instances.Count;
-    //   Orders = new List<Order> { };
-    // }
+    public Vendor(string vendorName)
+    {
+      Name = vendorName;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Orders = new List<Order> { };
+    }
 
     public static void ClearAll()
     {
